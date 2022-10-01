46e4defc276c6e1119ac048d018906ee9ac59d46
UserMarcus <114788534+UserMarcus@users.noreply.github.com>
2022-10-01 10:27:17 +0800

    Add files via upload

---
+++ TheCircle/TheCircle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double PI = 3.142;
+            double area, circumference;
+            Console.Write("Enter the radius of the Circle = ");
+            radius = double.Parse(Console.ReadLine());
+            area = PI * radius * radius;
+            circumference = 2 * PI * radius;
+            Console.WriteLine("The area of the Circle is = " + circumference);
+            Console.WriteLine("The circumference of the Circle is = " + area);
+            Console.ReadKey();
+
+        }
+    }
+}
